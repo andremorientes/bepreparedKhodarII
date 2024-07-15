b08bf9d97fa5e86f254396e458769817879f8eb3
André Morientes <59062594+andremorientes@users.noreply.github.com>
2024-07-15 23:37:15 +0200

    Initial commit

---
+++ src/main/java/com/morientesdev/beprepare/BeprepareApplication.java
@@ -0,0 +1,13 @@
+package com.morientesdev.beprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeprepareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeprepareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morientesdev/beprepare/BeprepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morientesdev.beprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeprepareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
